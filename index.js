58f4f9aa71dbe9a62c19a46c48786c09bfe07226
luisanegri <luisaanegri@gmail.com>
2019-11-24 15:08:56 +0100

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const cors = require('cors');
+const corsMiddleware = cors();
 const bodyParser = require('body-parser');
 const parserMiddleware = bodyParser.json();
 const postRouter = require('./posts/router');
 const userRouter = require('./user/router');
 const authRouter = require('./auth/router');
+app.use(corsMiddleware);
 app.use(parserMiddleware);
 app.use(authRouter);
 app.use(postRouter);
